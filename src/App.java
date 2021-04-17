2f4dd9ece2696113e63de84221e9cba53e672688
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-17 21:30:32 +0900

    Welcome user and create scanner

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, welcome to the Management System for the School of Options");
+
+        Scanner input = new Scanner(System.in);
     }
 }
