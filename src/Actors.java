a69b8d9d4c197e02fc841d2d6cc1a930e57b5453
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-17 21:44:06 +0900

    create constructor for the user class

--- src/Actors.java
+++ src/Actors.java
@@ -10,7 +10,22 @@ public class Actors {
         int phoneNumber;
 
         public Users() {
+            // Create scanner object
             Scanner input = new Scanner(System.in);
+
+            // Prompt user for information regarding who they are
+            System.out.println("What is your full name?");
+            fullName = input.nextLine();
+
+            System.out.println("What is your email?");
+            email = input.nextLine();
+
+            System.out.println("What is your phone number?");
+            phoneNumber = input.nextInt();
+
+            // close the scanner
+            input.close();
+
         }
 
     }
