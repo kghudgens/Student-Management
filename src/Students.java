9e0f54a812ce4c70bf25d7d26d810acc6f88be28
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-18 22:35:42 +0900

    override the updateStudent method

--- src/Students.java
+++ src/Students.java
@@ -3,4 +3,8 @@ public class Students {
     // Extends the user class
     // Possess the ability to R against student database
 
+    public static void updateStudent() {
+        throw new UnsupportedOperationException();
+    }
+
 }
