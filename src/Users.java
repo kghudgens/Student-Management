7304fb949103dcce605897ef9b8678cfae509c0a
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-18 22:55:10 +0900

    removed unnecessary code

--- src/Users.java
+++ src/Users.java
@@ -2,30 +2,6 @@ import java.util.Scanner;
 
 public class Users {
 
-    // Get general information
-    public String fullName;
-    public String email;
-    public int phoneNumber;
-
-    public Users() {
-        // Create scanner object
-        Scanner input = new Scanner(System.in);
-
-        // Prompt user for information regarding who they are
-        System.out.println("What is your full name?");
-        this.fullName = input.nextLine();
-
-        System.out.println("What is your email?");
-        this.email = input.nextLine();
-
-        System.out.println("What is your phone number?");
-        this.phoneNumber = input.nextInt();
-
-        // close the scanner
-        input.close();
-
-    }
-
     // all users can view students in the database
     public void viewStudent() {
         // View a student
