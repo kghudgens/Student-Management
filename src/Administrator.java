0427732976bbf57dd9eb823e05c92d30559a75dd
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-18 22:35:06 +0900

    Set template for functions only meant for admin

--- src/Administrator.java
+++ src/Administrator.java
@@ -2,5 +2,18 @@ public class Administrator extends Users {
 
     // Extends the user class
     // Possess the ability for CRUD against student database
+    /**
+     * Creates a new student in the database
+     */
+    private static void newStudent() {
+
+    }
+
+    /**
+     * Deletes the student
+     */
+    private static void deleteStudent() {
+
+    }
 
 }
