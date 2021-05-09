5b684e6731b5126c86c1f3cf20e3edef739c8e4b
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-09 14:24:43 +0900

    Created constructor method

--- src/Teachers.java
+++ src/Teachers.java
@@ -6,29 +6,34 @@ public class Teachers extends Users {
     public String name;
 
     // Get users name
-    System.out.println("What is your name?");
+    public Teachers() {
+        System.out.println("What is your name?");
 
-    this.name=input.nextLine();
+        this.name = input.nextLine();
 
-    // Dealing with the users choices
-    System.out.println("Welcome "+this.name+"as a teacher you have three options.");
-    // Present the teachers options
-    System.out.println("\n1. UPDATE a student's entry");System.out.println("VIEW a student's entry");System.out.println("VIEW all student's entry");
-    // Collect the teachers choice
-    int teachersChoice = input.nextLine();
+        // Dealing with the users choices
+        System.out.println("Welcome " + this.name + "as a teacher you have three options.");
+        // Present the teachers options
+        System.out.println("\n1. UPDATE a student's entry");
+        System.out.println("VIEW a student's entry");
+        System.out.println("VIEW all student's entry");
+        // Collect the teachers choice
+        int teachersChoice = input.nextLine();
 
-    // Deal with the users choice as the teacher
-    // Create an instance
-    switch(teachersChoice.toLowerCase()){
-        case "update":
-            // Call the update function
-            break;
+        // Deal with the users choice as the teacher
+        // Create an instance
+        switch (teachersChoice.toLowerCase()) {
+            case "update":
+                // Call the update function
+                break;
 
-        case "view":
-            // Call the view function
-            break;
-        case "view all":
-            // Call the view all function
-            break;
-}
+            case "view":
+                // Call the view function
+                break;
+            case "view all":
+                // Call the view all function
+                break;
+        }
+    }
+
 }
