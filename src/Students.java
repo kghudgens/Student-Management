04d0fca25d7f3ffde29b767f683f900976e3ca1d
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-09 14:13:16 +0900

    removed static keyword

--- src/Students.java
+++ src/Students.java
@@ -3,8 +3,8 @@ public class Students extends Users {
     // Extends the user class
     // Possess the ability to R against student database
 
-    public static void updateStudent() {
-        throw new UnsupportedOperationException();
+    public void updateStudent() {
+
     }
 
 }
