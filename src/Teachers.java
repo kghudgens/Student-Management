7e3fa4015cb8c5a24dda6e5ebf089dffccfc247b
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-09 14:23:21 +0900

    Created name variable and scanner

--- src/Teachers.java
+++ src/Teachers.java
@@ -1,12 +1,34 @@
 import java.util.Scanner;
 
 public class Teachers extends Users {
+    // Create scanner object
     Scanner input = new Scanner(System.in);
+    public String name;
 
-    // Show the administrator their permission
+    // Get users name
+    System.out.println("What is your name?");
 
-    // Collect the users choice
+    this.name=input.nextLine();
 
     // Dealing with the users choices
+    System.out.println("Welcome "+this.name+"as a teacher you have three options.");
+    // Present the teachers options
+    System.out.println("\n1. UPDATE a student's entry");System.out.println("VIEW a student's entry");System.out.println("VIEW all student's entry");
+    // Collect the teachers choice
+    int teachersChoice = input.nextLine();
 
+    // Deal with the users choice as the teacher
+    // Create an instance
+    switch(teachersChoice.toLowerCase()){
+        case "update":
+            // Call the update function
+            break;
+
+        case "view":
+            // Call the view function
+            break;
+        case "view all":
+            // Call the view all function
+            break;
+}
 }
