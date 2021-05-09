40ed00ae384124921e55913a08de8a592aeffbfa
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-09 14:04:29 +0900

    provide users with menu

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 
 public class Administrator extends Users {
+
+    public String fullName;
     Scanner input = new Scanner(System.in);
 
-    // Extends the user class
-    // Possess the ability for CRUD against student database
-    /**
-     * Creates a new student in the database
-     */
-
     public Administrator() {
+        // Get name
+        System.out.println("What is your name? ");
+        this.fullName = input.nextLine();
+        System.out.println("Welcome " + this.fullName + " as the administrator you have four options.");
+
+        // Provide user options
+        System.out.println("Select from the four:");
+        System.out.println(
+                "\n1. NEW student entry\n 2. UPDATE a student's entry \n3. VIEW a student's entry \n 4. VIEW ALL student entries \n5. DELETE a student's entry");
 
     }
 
