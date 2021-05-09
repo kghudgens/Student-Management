2c6a1ce84793708dc39b3cc2794dd4f469e62e39
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-09 14:37:21 +0900

    import and create scanner

--- src/Students.java
+++ src/Students.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Students extends Users {
+    Scanner input = new Scanner(System.in);
 
     // Extends the user class
     // Possess the ability to R against student database
     // Update only their entry
 
     public Students() {
+
         System.out.println("Welcome as a student you have two options.");
         // Present the students options
         System.out.println("VIEW a student's entry");
