5a8bda8f9dd6b23ada0a808ccffcf099b2de2947
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-09 14:36:12 +0900

    Created constructor and additional methods

--- src/Students.java
+++ src/Students.java
@@ -2,8 +2,31 @@ public class Students extends Users {
 
     // Extends the user class
     // Possess the ability to R against student database
+    // Update only their entry
+
+    public Students() {
+        System.out.println("Welcome as a student you have two options.");
+        // Present the students options
+        System.out.println("VIEW a student's entry");
+        System.out.println("VIEW all student's entry");
+
+        String studentChoice = input.nextLine();
+        // Deal with the users choice
+        switch (studentChoice.toLowerCase()) {
+            case "view":
+
+                break;
+            case "view all":
+
+                break;
+        }
+    }
 
     public void updateStudent() {
+
+    }
+
+    public void viewStudent() {
 
     }
 
