d3a82602f081ab0b390d8fb5923666411394b987
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-10 20:33:09 +0900

    Added the attributes of students to parent class

--- src/Users.java
+++ src/Users.java
@@ -1,4 +1,14 @@
 public class Users {
+    public String firstName;
+    public String lastName;
+    public String goal;
+    public String phoneNumber;
+    public String classOne;
+    public String classTwo;
+    public String classThree;
+    public String classFour;
+    public String favoriteSubject;
+    public int studentID;
 
     // all users can view students in the database
     public void viewStudent() {
