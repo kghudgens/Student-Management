88a3cc412b96510815330e79d11b551991a01306
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-11 20:56:36 +0900

    created name function

--- src/Users.java
+++ src/Users.java
@@ -2,11 +2,28 @@ import java.util.Scanner;
 
 public class Users {
 
+    /**
+     * Function that gets the unique student ID to match them to database
+     */
     public void getStudentID() {
         // Will be reused
         System.out.println("Please enter the five digit Student Identification Number?");
         this.studentID = input.nextInt();
         input.nextLine();
+    }
+
+    /**
+     * Function that gets the name of the student to set in the classes attributes
+     */
+    public void getName() {
+        System.out.println("What is the first name of the student?");
+        this.firstName = input.nextLine();
+        System.out.println("What is the last name of the student?");
+        this.lastName = input.nextLine();
+    }
+
+    public void getGoal() {
+
     }
 
     Scanner input = new Scanner(System.in);
@@ -25,12 +42,12 @@ public class Users {
     // all users can view students in the database
     public void viewStudent() {
         getStudentID();
-        System.out.println(this.studentID);
+
         // Prepared statement
     }
 
     public void updateStudent() {
-
+        getStudentID();
     }
 
     public void viewAllStudent() {
