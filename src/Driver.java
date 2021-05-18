7cd8a37da03c5e86dcb766b1352bd0bd04d98ae4
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-18 16:08:57 +0900

    import sql lib and removed static

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,15 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class Driver {
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
 
-    public static void connectDatabase() {
-        Connection conn = null;
+    public void connectDatabase() {
 
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_management");
