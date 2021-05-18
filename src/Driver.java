219f6381839f5f4bdb0aab662ede970200b48a32
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-18 16:04:25 +0900

    Connect to database function created

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Driver {
 
+    public static void connectDatabase() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_management");
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
 }
