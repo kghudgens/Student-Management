a1c0237e39f6789b89cf1f818e355462ea8d19af
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-18 16:09:13 +0900

    Create instance of driver

--- src/App.java
+++ src/App.java
@@ -10,7 +10,9 @@ public class App {
                 "This program connects to the School Management System's Database and gives the user access to data of the students and Staff.\n");
 
         // Call function that connects to database
-        Driver.connectDatabase();
+        Driver driver = new Driver();
+
+        driver.connectDatabase();
 
         // How will the user be using the program
         Scanner input = new Scanner(System.in);
