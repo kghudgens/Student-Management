27b28e5c6ca66e0e6a695ca6b21f12aadb7b5a7a
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-18 15:46:21 +0900

    Removed function that belongs in driver

--- src/App.java
+++ src/App.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
 
 public class App {
-
-    public static void connectDatabase() {
-
-    }
-
     public static void main(String[] args) {
         System.out.println("\nHello, welcome to the Management System for the School of Options");
 
