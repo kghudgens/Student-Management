9ac9906ca053b1383d3b5d6cd90d22953c604db0
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-31 21:50:32 +0900

    moved methods into student class

--- src/Users.java
+++ src/Users.java
@@ -10,9 +10,11 @@ public class Users {
      * Instance of the student for the administrator to submit to database
      */
     public class Student {
+        // Attributes that define the student instance
         private String studentFirstName;
         private String studentLastName;
         private int studentID;
+        private String phoneNumber;
 
         /**
          * Initialize student instance
@@ -33,46 +35,40 @@ public class Users {
         /**
          * sets the student ID for the student instance
          */
-        private void setStudentID() {
+        public void setStudentID() {
             System.out.println("Student ID:");
             this.studentID = input.nextInt();
             input.nextLine();
         }
 
-    }
+        /**
+         * Function that gets the unique student ID to match them to database
+         */
+        public void getStudentID() {
+            // Will be reused
+            System.out.println("Please enter the five digit Student Identification Number?");
+            this.studentID = input.nextInt();
+            input.nextLine();
+        }
 
-    public String firstName;
-    public String lastName;
-    public String phoneNumber;
-    public int studentID;
+        /**
+         * Function that gets the name of the student to set in the classes attributes
+         */
+        public void getName() {
+            System.out.println("What is the first name of the student?");
+            this.studentFirstName = input.nextLine();
+            System.out.println("What is the last name of the student?");
+            this.studentLastName = input.nextLine();
+        }
 
-    /**
-     * Function that gets the unique student ID to match them to database
-     */
-    public void getStudentID() {
-        // Will be reused
-        System.out.println("Please enter the five digit Student Identification Number?");
-        this.studentID = input.nextInt();
-        input.nextLine();
-    }
-
-    /**
-     * Function that gets the name of the student to set in the classes attributes
-     */
-    public void getName() {
-        System.out.println("What is the first name of the student?");
-        this.firstName = input.nextLine();
-        System.out.println("What is the last name of the student?");
-        this.lastName = input.nextLine();
-    }
-
-    /**
-     * Function that gets the phone number of the student, sets it in the attributes
-     * and gets it staged for the upcoming prepared statement
-     */
-    public void number() {
-        System.out.println("What is the students phone number?");
-        this.phoneNumber = input.nextLine();
+        /**
+         * Function that gets the phone number of the student, sets it in the attributes
+         * and gets it staged for the upcoming prepared statement
+         */
+        public void setNumber() {
+            System.out.println("What is the students phone number?");
+            this.phoneNumber = input.nextLine();
+        }
     }
 
     // all users can view students in the database
@@ -83,6 +79,7 @@ public class Users {
         // students information
     }
 
+    // method that queries for all school information
     public void viewSchoolInformation() {
         driver.viewSchool();
 
